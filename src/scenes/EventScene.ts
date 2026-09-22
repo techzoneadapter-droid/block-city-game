937b5676a0afb06204c8d54997bd51ea3673190a
techzoneadapter-droid <techzone.adapter@gmail.com>
2026-09-22 23:14:49 +0700

    Mark EventScene.ts as v0.8

--- src/scenes/EventScene.ts
+++ src/scenes/EventScene.ts
@@ -45,7 +45,7 @@ export class EventScene extends Phaser.Scene {
     this.createMilestones();
     this.createWaysToEarn();
 
-    this.add.text(W - 22, 813, "v0.7", {
+    this.add.text(W - 22, 813, "v0.8", {
       fontFamily: "Inter, system-ui",
       fontSize: "8px",
       fontStyle: "bold",
