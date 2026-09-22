e3f16470188db7b9333316a7f15eea868e8a6dae
techzoneadapter-droid <techzone.adapter@gmail.com>
2026-09-22 23:08:02 +0700

    Register campaign journey scene

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { CityScene } from "./scenes/CityScene";
 import { DailyScene } from "./scenes/DailyScene";
 import { ProgressScene } from "./scenes/ProgressScene";
 import { EventScene } from "./scenes/EventScene";
+import { CampaignScene } from "./scenes/CampaignScene";
 import { H, W } from "./ui";
 
 const config: Phaser.Types.Core.GameConfig = {
@@ -32,7 +33,7 @@ const config: Phaser.Types.Core.GameConfig = {
     transparent: false,
     powerPreference: "high-performance",
   },
-  scene: [HomeScene, PuzzleScene, CityScene, DailyScene, ProgressScene, EventScene],
+  scene: [HomeScene, CampaignScene, PuzzleScene, CityScene, DailyScene, ProgressScene, EventScene],
 };
 
 new Phaser.Game(config);
