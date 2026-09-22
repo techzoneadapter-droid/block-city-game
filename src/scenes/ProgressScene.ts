4cdd1c2bb4d2342521576c2bb3d8936f6d4293c3
techzoneadapter-droid <techzone.adapter@gmail.com>
2026-09-22 23:14:52 +0700

    Mark ProgressScene.ts as v0.8

--- src/scenes/ProgressScene.ts
+++ src/scenes/ProgressScene.ts
@@ -48,7 +48,7 @@ export class ProgressScene extends Phaser.Scene {
     this.createMilestone(milestone.title, milestone.progress, milestone.target, milestone.text);
     this.createAchievements();
 
-    this.add.text(W - 22, 813, "v0.7", {
+    this.add.text(W - 22, 813, "v0.8", {
       fontFamily: "Inter, system-ui",
       fontSize: "8px",
       fontStyle: "bold",
