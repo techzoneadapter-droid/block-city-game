3b01c37fb830cb4cf650012b6e4b5631eeb463d2
techzoneadapter-droid <techzone.adapter@gmail.com>
2026-09-22 23:14:46 +0700

    Mark DailyScene.ts as v0.8

--- src/scenes/DailyScene.ts
+++ src/scenes/DailyScene.ts
@@ -49,7 +49,7 @@ export class DailyScene extends Phaser.Scene {
     this.createMissionCards();
     this.createChest();
 
-    this.add.text(W - 22, 813, "v0.7", {
+    this.add.text(W - 22, 813, "v0.8", {
       fontFamily: "Inter, system-ui",
       fontSize: "8px",
       fontStyle: "bold",
